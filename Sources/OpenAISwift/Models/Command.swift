70e5573b3e4fe0ebcf76128c544d79a1d057619a
Sara Hender <shender@tubi.tv>
2023-01-05 19:24:04 -0800

    Add more JSON parameters

--- Sources/OpenAISwift/Models/Command.swift
+++ Sources/OpenAISwift/Models/Command.swift
@@ -8,16 +8,22 @@ class Command: Encodable {
     var prompt: String
     var model: String
     var maxTokens: Int
-    
-    init(prompt: String, model: String, maxTokens: Int) {
+    var n: Int
+    let presencePenalty = 2.0
+    let frequencyPenalty = 2.0
+
+    init(prompt: String, model: String, maxTokens: Int, n: Int) {
         self.prompt = prompt
         self.model = model
         self.maxTokens = maxTokens
+        self.n = n
     }
     
     enum CodingKeys: String, CodingKey {
         case prompt
         case model
         case maxTokens = "max_tokens"
+        case presencePenalty = "presence_penalty"
+        case frequencyPenalty = "frequency_penalty"
     }
 }
